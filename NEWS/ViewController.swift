8ac800a69e2f3913968ca7a7b8b092752391b932
Nadeesha Lakmal <nadeeshal@codegen.net>
2019-12-14 21:13:36 +0530

    * Create project

---
+++ NEWS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NEWS
+//
+//  Created by Nadeesha Lakmal on 14/12/2019.
+//  Copyright © 2019 Nadeesha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
